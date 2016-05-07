4660de2ffb42bc5864cd78aa7a6bd1e9741add8b
Andrew Wagner <wagner.andrew@gmail.com>
2016-05-07 18:58:02 -0400

    more mate-in-1 problems. a bit of styling

--- assets/script/mini-mates.js
+++ assets/script/mini-mates.js
@@ -22,10 +22,16 @@ var app = angular.module("myApp", []).controller("SimpleController", function($s
 
   $scope.showPosition = function(fen, solution){
       $scope.board = new Chess(fen);        
+      if (fen.includes(" b ")) {
+	$scope.color = "Black";
+      } else {
+	$scope.color = "White";
+      }
       $scope.chessBoard = ChessBoard('board1', {
 	      position: fen,
 	      pieceTheme: 'script/chessboard.js/img/chesspieces/wikipedia/{piece}.png',
 	      draggable: true,
+	      orientation: $scope.color.toLowerCase(),
 	      dropOffBoard: 'snapback',
 	      showNotation: true,
 	      showErrors: 'alert',
@@ -34,9 +40,16 @@ var app = angular.module("myApp", []).controller("SimpleController", function($s
 		  
 		  var move = $scope.board.move({from: source, to: target});
 		  if (move == null) return 'snapback';
-		  if (move.san == (solution + "#")){
-		      $scope.count += 1;;
-		      $scope.$apply();
+
+		  var solutionMoves = solution.split(" ");
+		  for (var i = 0; i < solutionMoves.length; i++){
+		      var solutionMove = solutionMoves[i];
+
+		      if ((move.san == (solutionMove + "#")) || (("" + move.from + move.to) == solutionMove)){
+			  $scope.count += 1;;
+			  $scope.$apply();
+			  break;
+		      }
 		  }
 		  $scope.showRandomPosition();
 	      }
@@ -45,7 +58,6 @@ var app = angular.module("myApp", []).controller("SimpleController", function($s
 
   $scope.done = function(){
       done = true;
-      $("#board1").hide();
       console.log("you finished " + $scope.count + " in 30 seconds");
       alert("you finished " + $scope.count + " in 30 seconds!");
   };
