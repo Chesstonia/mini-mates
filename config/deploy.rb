c1a9c93b86d859c6b05214593cbcefe1df24d94b
Andrew Wagner <wagner.andrew@gmail.com>
2016-05-08 13:11:26 -0400

    hook up capistrano for deployments

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'mini-mates'
+set :repo_url, 'https://github.com/Chesstonia/mini-mates.git'
+
+set :use_sudo, true
+
+set :pty, true
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: ["publickey"],
+  keys: ["~/bots.pem"]
+}
+
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '54.211.213.225', user: 'ubuntu', roles: %w{web}
